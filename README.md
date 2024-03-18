973cf656621703591cbe1f849475ebd405b1f34a
Mohammed Hoosain <styphoiz@gmail.com>
2024-03-18 13:36:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Valr Staking Automation Script
+
+This script automates the staking of SOL and AVAX on the Valr platform at one-hour intervals.
+
+## Getting Started
+
+Prerequisites:
+
+Node.js and npm (or yarn) installed
+A Valr account with API credentials
+Installation:
+
+Clone this repository or download the script.
+
+Install dependencies:
+
+Bash  
+```
+npm install dotenv crypto node-fetch@2
+```
+Use code with caution.  
+
+Configuration:  
+Rename .env_sample to .env  
+
+##Set up your API keys with the following configs:  
+View access, Trade, Internal Transfer
+
+Update the following environment variables, replacing placeholders with your Valr API credentials:
+VALR_API_KEY_STAKING=your_api_key  
+VALR_SECRET_STAKING=your_api_secret  
+
+Update the below as needed for the specified product staking:  
+SOLSTAKE=true  
+AVAXSTAKE=true  
+
+##Running the Script:
+
+###Start the script:
+
+Bash
+```
+node staking.js
+```
+Use code with caution.
+## Script Functionality:
+
+API Authentication: Uses HMAC SHA512 to sign API requests for security.  
+Balance Retrieval: Employs getBalance function to fetch available balances for specified currencies.  
+Staking: Utilizes stake function to initiate staking for SOL and AVAX (configurable using SOLSTAKE and AVAXSTAKE flags).  
+Interval Execution: Runs cyclically with a one-hour interval for continuous staking.  
+Logging: Provides informative messages for successful staking or failure alerts.  
+## Important Notes:
+
+Educational Purposes Only: Not intended for production environments without thorough testing and error handling.  
+Inherent Risks: Staking cryptocurrency involves risks. Understand potential risks before usage.  
+Staking Entire Balance: Currently stakes the entire available balance for specified currencies. Modify as needed for partial staking.  
+For further details, refer to the in-code comments and documentation.  
+
+## Tips
+
+If you found this script helpful, you can show your appreciation by sending a tip to Mohammed using Valr Pay at 6SXBAT989R8HDV3DAU3Z.
---
+++ staking.js
@@ -0,0 +1,138 @@
+const dotenv = require("dotenv");
+const crypto = require("crypto");
+const fetch = require("node-fetch"); // Require node-fetch for Node.js make sure this is node-fetch@2
+dotenv.config();
+
+const apiKey = process.env.VALR_API_KEY_STAKING;
+const secret = process.env.VALR_SECRET_STAKING;
+const SOLSTAKE = true;
+const AVAXSTAKE = true;
+
+async function getBalance(currency, fixed, roundDown = false) {
+  const url = `https://api.valr.com/v1/account/balances?excludeZeroBalances=true`;
+
+  // Get the current timestamp in milliseconds
+  const timestamp = Date.now();
+
+  // Create the X-VALR-SIGNATURE header using the signRequest function
+  const signature = crypto
+    .createHmac("sha512", secret)
+    .update(timestamp.toString())
+    .update("GET")
+    .update(`/v1/account/balances?excludeZeroBalances=true`)
+    .digest("hex");
+
+  const headers = new fetch.Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("X-VALR-API-KEY", apiKey);
+  headers.append("X-VALR-SIGNATURE", signature);
+  headers.append("X-VALR-TIMESTAMP", timestamp.toString());
+
+  const requestOptions = {
+    method: "GET",
+    headers,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch(url, requestOptions);
+    if (!response.ok) {
+      throw new Error(`API Error - Status Code: ${response.status}`);
+    }
+    const result = await response.text();
+    const jsonResult = JSON.parse(result).filter((a) => a.currency == currency);
+    if (roundDown) {
+      return (
+        Math.floor(parseFloat(jsonResult[0].available) * Math.pow(10, fixed)) /
+        Math.pow(10, fixed)
+      );
+    } else {
+      return parseFloat(jsonResult[0].available).toFixed(fixed);
+    }
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw error;
+  }
+}
+
+async function stake(currencySymbol, value) {
+  const url = `https://api.valr.com/v1/staking/stake`;
+  const amount = value.toString();
+
+  // Get the current timestamp in milliseconds
+  const timestamp = Date.now();
+
+  //Create the request body
+  const requestBody = JSON.stringify({
+    currencySymbol,
+    amount,
+  });
+
+  // Create the X-VALR-SIGNATURE header using the signRequest function
+  const signature = crypto
+    .createHmac("sha512", secret)
+    .update(timestamp.toString())
+    .update("POST")
+    .update(`/v1/staking/stake`)
+    .update(requestBody)
+    .digest("hex");
+
+  const headers = new fetch.Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("X-VALR-API-KEY", apiKey);
+  headers.append("X-VALR-SIGNATURE", signature);
+  headers.append("X-VALR-TIMESTAMP", timestamp.toString());
+
+  const requestOptions = {
+    method: "POST",
+    headers,
+    body: requestBody,
+  };
+
+  try {
+    const response = await fetch(url, requestOptions);
+    if (!response.ok) {
+      console.error(`API Error - Status Code: ${response.status}`);
+    }
+    return response;
+  } catch (error) {
+    console.error("Error:", error.message);
+    return response;
+  }
+}
+async function runInterval1() {
+  try {
+    if (SOLSTAKE) {
+      const solBal = await getBalance("SOL", 8, true);
+      console.log(`[${new Date().toISOString()}] Staking SOL: ${solBal} SOL`);
+      const solStakeResponse = await stake("SOL", solBal);
+      if (solStakeResponse.ok) {
+        console.log(`[${new Date().toISOString()}] SOL staked successfully`);
+      } else {
+        console.log(`[${new Date().toISOString()}] Failed to stake SOL`);
+      }
+    }
+    if (AVAXSTAKE) {
+      const avaxBal = await getBalance("AVAX", 8, true);
+      console.log(
+        `[${new Date().toISOString()}] Staking AVAX: ${avaxBal} AVAX`
+      );
+      const avaxStakeResponse = await stake("AVAX", avaxBal);
+      if (avaxStakeResponse.ok) {
+        console.log(`[${new Date().toISOString()}] AVAX staked successfully`);
+      } else {
+        console.log(`[${new Date().toISOString()}] Failed to stake AVAX`);
+      }
+    }
+  } catch (error) {
+    console.error("Error:", error.message);
+    // Handle the error gracefully
+  }
+
+  setTimeout(() => {
+    runInterval1();
+  }, 3600000); // Run every hour
+}
+
+// Start the intervals immediately
+runInterval1(); // Initial trigger for runInterval1
